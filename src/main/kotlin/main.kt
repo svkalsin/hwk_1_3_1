9e157f69a7b36d5a915692d272bef37086155ba7
Sergey Kalsin <ksvk4@mail.ru>
2020-11-02 22:01:34 +0300

    Доработал

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,7 @@ package ru.netology
 
 fun main() {
 
-    val numberOfSeconds = 60 //количество пройденого времени в секундах
+    val numberOfSeconds = 60*60*11 //количество пройденого времени в секундах
     declination(numberOfSeconds)
 }
 
@@ -11,33 +11,25 @@ fun declination(numberOfSeconds: Int) {
     if (numberOfSeconds <= 60) {
         print("был(а) только что")
     } else if (numberOfSeconds >= 61 && numberOfSeconds < 60*60){
-        print("был(а) ${numberOfSeconds/60} ${declinationMinutes(numberOfSeconds)} назад")
+        print("был(а) ${numberOfSeconds/60} ${declinationMinutesHours(numberOfSeconds, true)} назад")
     } else if (numberOfSeconds >= 60*60 && numberOfSeconds < 24*60*60){
-        print("был(а) ${numberOfSeconds/(60*60)} ${declinationHours(numberOfSeconds)} назад")
+        print("был(а) ${numberOfSeconds/(60*60)} ${declinationMinutesHours(numberOfSeconds, false)} назад")
     } else if (numberOfSeconds >= 24*60*60){
         print("был(а) ${declinationDay(numberOfSeconds)}")
     }
 
 }
 
-fun declinationMinutes(numberOfSeconds: Int) :String {
+fun declinationMinutesHours(numberOfSeconds: Int, showMinutes: Boolean = true) :String {
     val strMinutes = (numberOfSeconds/60).toInt().toString()
     var str: String = ""
     when (strMinutes.substring(strMinutes.lastIndex)){
-        "0","5","6","7","8","9" -> str = "минут"
-        "1" -> str = "минута"
-        "2", "3", "4" -> str = "минуты"
+        "0","5","6","7","8","9" -> str = if (showMinutes) "минут" else "часов"
+        "1" -> str = if (showMinutes) "минута" else "час"
+        "2", "3", "4" -> str = if (showMinutes) "минуты" else "часа"
     }
-    return str
-}
-
-fun declinationHours(numberOfSeconds: Int) :String {
-    val strHours = (numberOfSeconds/(60*60)).toInt().toString()
-    var str: String = ""
-    when (strHours.substring(strHours.lastIndex)){
-        "0","5","6","7","8","9" -> str = "часов"
-        "1" -> str = "час"
-        "2", "3", "4" -> str = "часа"
+    when (strMinutes.substring(strMinutes.lastIndex-1)){
+        "11","13","14","15","16","17","18","19" -> str = if (showMinutes) "минут" else "часов"
     }
     return str
 }
